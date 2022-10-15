eabb8311565d1690cb7488751dd4e29fb6df1ec8
estilbee <odaouphars@gmail.com>
2022-10-15 17:04:35 -0400

    added forecast card with inital weather info

---
+++ script.js
@@ -0,0 +1,62 @@
+var searchButton = document.getElementById("search-button")
+
+searchButton.addEventListener("click",function(){
+    var searchValue = document.querySelector("#input-box").value
+getApi(searchValue)
+})
+
+function getApi(inputValue){
+var requestUrl= `http://api.openweathermap.org/geo/1.0/direct?q=${inputValue}&appid=7ff95d427ec7a429b5759073047c96ef`;
+fetch(requestUrl)
+.then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    for (i = 0; i < data.length; i++) {
+        console.log(data[i].lat);
+        console.log(data[i].lon);
+      }
+      getApi2(data[0].lat,data[0].lon);
+      getApi3(data[0].lat,data[0].lon);
+  });
+}
+
+function getApi2(lat,lon){
+    var requestUrl2 = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=7ff95d427ec7a429b5759073047c96ef&units=imperial`
+    fetch(requestUrl2)
+    .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);  
+        
+        for( i = 4; i < data.list.length; i=i+8){ //i + 8 is telling it to iterate over every 24 hours for 5 days
+            console.log(data.list[i])
+            var forecastCard = $("<div>").addClass("card")
+            var temp = $("<h2>").text("temp: " + data.list[i].main.temp)
+            var humidity = $("<h2>").text("humidity:" + data.list[i].main.humidity)
+            var date = $("<h2>").text(moment.unix(data.list[i].dt).format("LL"))
+
+
+
+
+            forecastCard.append(temp, humidity, date)
+        $("#forecast").append(forecastCard)
+        }
+    });
+}
+
+
+function getApi3(lat,lon){
+    var requestUrl3 = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=7ff95d427ec7a429b5759073047c96ef`
+    fetch(requestUrl3)
+    .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);  
+
+    });
+}
+
